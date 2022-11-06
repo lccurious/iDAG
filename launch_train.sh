defb1641a3a09bf979ff9867d1fa8e6357431b05
lccurious <lccurious@outlook.com>
2022-11-07 03:49:39 +0800

    update lauch script

---
+++ launch_train.sh
@@ -0,0 +1,10 @@
+python -m domainbed.scripts.sweep launch \
+       --data_dir=/home/hzn/datasets \
+       --output_dir=train_out/sweep/officehome \
+       --command_launcher multi_gpu \
+       --algorithms DAGDG \
+       --datasets OfficeHome \
+       --single_test_envs \
+       --n_hparams 5 \
+       --n_trials 1 \
+       --hparams '{"resnet18": "True"}'
